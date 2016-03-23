154bc4bd63236bb4f03490f7b3ed57906d7d313a
root <root@dev.galotecnia.com>
2016-03-23 22:19:52 +0100

    1a version

---
+++ settings.py
@@ -0,0 +1,7 @@
+REDMINE_PUBLIC_URL = 'http://example.site.com/'
+REDMINE_API_URL = 'http://localhost/'
+REDMINE_KEY = 'changeme'
+TELEGRAM_TOKEN = 'changeme'
+DEFAULT_ACTIVITY = 1
+
+from settings_local import *
